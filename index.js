04871dfe5dc73ba5ed5e70af0d600d7a35c5b602
Lucas Bezerra <1lcs.bzrr@gmail.com>
2023-03-16 15:14:42 -0300

    feat: google maps

--- index.js
+++ index.js
@@ -2,10 +2,33 @@ require("dotenv").config();
 const cors = require("cors");
 const express = require("express");
 const app = express();
-const CepCoords = require("coordenadas-do-cep");
+
+const axios = require("axios");
+const { Client } = require("@googlemaps/google-maps-services-js");
+const client = new Client({});
 
 app.use(express.json());
 app.use(cors());
+
+async function calculateDistance(address1, address2) {
+  try {
+    const response = await client.distancematrix({
+      params: {
+        origins: [address1],
+        destinations: [address2],
+        key: process.env.GOOGLE_MAPS_API_KEY,
+      },
+      timeout: 1000, // milliseconds
+      axiosInstance: axios,
+    });
+
+    const distance = response.data.rows[0].elements[0].distance.value;
+    return distance;
+  } catch (error) {
+    console.log(error);
+    return null;
+  }
+}
 
 app.get("/", (req, res) => {
   res.status(200).send("Funcionou chupetinha! 👌").end();
@@ -18,7 +41,7 @@ app.post("/calcular-distancia", async (req, res) => {
   };
 
   try {
-    const distance = await CepCoords.getDistEntreCeps(data.start, data.end);
+    const distance = await calculateDistance(data.start, data.end);
     res.status(200).json({
       distance,
     });
